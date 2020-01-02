d9bb1a7edd58c211da908ea47d3933a511bd3306
Ender Temel <ender@medusamedya.com>
2020-01-02 18:23:19 +0300

    Senaryo eklendi.

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState ;
+
+
+    State state; // State sınıfının referansını kullanarak state adından bir değişken oluşturuyoruz.
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState; 
+        textComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+        
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+
+        for(int index = 0 ; index < nextStates.Length;index++)
+        {
+            if(Input.GetKeyDown(KeyCode.Alpha1 + index ))
+        {
+            state = nextStates[index];
+        }
+
+        }
+
+         if (Input.GetKeyDown(KeyCode.Q))
+        {
+            state = startingState;
+        }  
+        
+        textComponent.text = state.GetStateStory();
+        }
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[CreateAssetMenu( menuName = "State", order = 0)]
+public class State : ScriptableObject {
+    
+   [TextArea(14,10) ] [SerializeField]  string storyText; // Text area ile story Textimizin boyutunu belirliyoruz . Degiskenkleri min lines , max lines 
+   [SerializeField] State[] nextStates ;
+
+   public  string GetStateStory ()
+   {
+      return storyText;
+   }
+
+   public State[] GetNextStates()
+   {
+      return nextStates ;
+   }
+}
+
+
